3c90239fc2d526496017c53ccc4960f84dfaae87
hbar1st <35541470+hbar1st@users.noreply.github.com>
2025-08-09 17:31:33 -0400

    Save progress: 760.1

--- insert_data.sh
+++ insert_data.sh
@@ -4,22 +4,22 @@
 PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only -c"
 cat courses_test.csv | while IFS="," read MAJOR COURSE
 do
-if [[ $MAJOR != major ]]
-then
-#get major_id
-MAJOR_ID=$($PSQL "select major_id from majors where major='$MAJOR'")
-#if not found
-if [[ -z $MAJOR_ID ]]
-then
-#insert major
-INSERT_MAJOR_RESULT=$($PSQL "insert into majors(major) values('$MAJOR')")
-echo $INSERT_MAJOR_RESULT
-#get new major_id
-fi
-#get course_id
-#if not found
-#insert course
-#get new course_id
-#insert into majors_courses
-fi
+  if [[ $MAJOR != major ]]
+  then
+  #get major_id
+  MAJOR_ID=$($PSQL "select major_id from majors where major='$MAJOR'")
+  #if not found
+  if [[ -z $MAJOR_ID ]]
+  then
+    #insert major
+    INSERT_MAJOR_RESULT=$($PSQL "insert into majors(major) values('$MAJOR')")
+    echo $INSERT_MAJOR_RESULT
+    #get new major_id
+  fi
+  #get course_id
+  #if not found
+  #insert course
+  #get new course_id
+  #insert into majors_courses
+  fi
 done
